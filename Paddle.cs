9bae7635428fe1c24cce212fbe0fe8c30870ea6c
AlexH-LBS <alexander.holmstedt@elev.ga.lbs.se>
2025-10-16 14:37:45 +0200

    Del1 klart

---
+++ Paddle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb2_ConsolePong
+{
+    internal class Paddle
+    {
+        int x;
+        int y;
+        int size;
+
+        public Paddle(int x, int y, int size)
+        {
+            x = this.x;
+            y = this.y;
+            size = this.size;
+        }
+        void Move(int moveValue)
+        {
+            y += moveValue;
+        }
+
+        void Draw()
+        {
+            for (int i = 0; i > size; i++)
+            {
+                Console.SetCursorPosition(x, y+i);
+                Console.Write("O");
+            }
+        }
+    }
+}
